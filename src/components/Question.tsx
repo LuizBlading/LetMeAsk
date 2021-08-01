6621f7854eb8a1d1e0be856bd37b030f613227f3
LuizBlading <luizfelipe1342@yahoo.com.br>
2021-08-01 20:45:48 -0300

    style like button

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,16 +1,20 @@
-import "../styles/question.scss"
+import { ReactNode } from 'react';
+
+import "../styles/question.scss";
 
 type QuestionProps = {
     content: string;
     author: {
         name: string;
         avatar: string
-    }
+    };
+    children?: ReactNode;
 }
 
 export function Question({
     content,
     author,
+    children,
 }: QuestionProps){
     return(
         <div className="question">
@@ -21,7 +25,9 @@ export function Question({
                     <span>{author.name}</span>
                 </div>
 
-                <div></div>
+                <div>
+                    {children}
+                </div>
             </footer>
         </div>
     );
